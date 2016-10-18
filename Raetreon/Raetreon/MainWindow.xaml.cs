4f472890ef6ae47700ccf2cb16bfc1292ecafaf6
BBalint94 <balint1994@gmail.com>
2016-10-18 16:49:36 +0200

    Project létrehozása, menürendszer tervezése
    3 button elhelyezése a MainWindowon: 1. megjelenít egy újabb ablakot új
    karakter létrehozásához, közben elrejti a MainWindow-ot, de van
    lehetőség a visszalépésre, 2. szintén megjelenít egy új ablakot a
    korábbi karakter betöltéséhez, 3. bezárja az alkalmazást.

---
+++ Raetreon/Raetreon/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Raetreon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ujjatek_Click(object sender, RoutedEventArgs e)
+        {
+            Ujjatek uj = Ujjatek.getInstance();
+            uj.AblakMegjelenit();
+            this.Close();
+        }
+
+        private void betolt_Click(object sender, RoutedEventArgs e)
+        {
+            Betoltes be = Betoltes.getInstance();
+            be.AblakMegjelenit();
+            this.Close();
+        }
+
+        private void kilepes_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
