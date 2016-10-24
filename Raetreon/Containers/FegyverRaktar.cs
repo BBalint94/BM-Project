ca197786187c9231dcf4604b3652246620a9bce5
BBalint94 <balint1994@gmail.com>
2016-10-24 23:11:24 +0200

    Konténer osztályok létrehozása
    - Fegyverek számára konténer osztály készítése (FegyverRaktar) :
    listákat tartalmaz a fegyvereknek típus szerint, illetve a listákhoz
    való hozzáadásoknak az eljárásait
    - Karaktereknek konténer osztály (KarakterRaktar) : listákat tartalmaz a
    karaktereknek kaszt szerint, illetve a listákhoz való hozzáadásoknak az
    eljárásait

---
+++ Raetreon/Containers/FegyverRaktar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raetreon
+{
+    public class FegyverRaktar
+    {
+        private static FegyverRaktar uniqueInstance = null;
+        private FegyverRaktar() { }
+        public static FegyverRaktar getInstance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new FegyverRaktar();
+            return uniqueInstance;
+        }
+        private List<KozelharciF> _khfegyverek = new List<KozelharciF>();
+        public List<KozelharciF> khfegyverek
+        {
+            get { return _khfegyverek; }
+        }
+        private List<TavolsagiF> _tavfegyverek = new List<TavolsagiF>();
+
+        public List<TavolsagiF> tavfegyverek
+        {
+            get { return _tavfegyverek; }
+        }
+        private List<MagikusF> _magfegyverek = new List<MagikusF>();
+        public List<MagikusF> magfegyverek
+        {
+            get { return _magfegyverek; }
+        }
+        public void KhFegyverHozzaad(KozelharciF fegyver)
+        {
+            if (!_khfegyverek.Contains(fegyver))
+                khfegyverek.Add(fegyver);
+        }
+        public void TavFegyverHozzaad(TavolsagiF fegyver)
+        {
+            if (!_tavfegyverek.Contains(fegyver))
+                tavfegyverek.Add(fegyver);
+        }
+        public void MagFegyverHozzaad(MagikusF fegyver)
+        {
+            if (!_magfegyverek.Contains(fegyver))
+                magfegyverek.Add(fegyver);
+        }
+    }
+}
---
+++ Raetreon/Containers/KarakterRaktar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raetreon.Containers
+{
+    class KarakterRaktar
+    {
+        private static KarakterRaktar uniqueInstance = null;
+        private KarakterRaktar() { }
+        public static KarakterRaktar getInstance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new KarakterRaktar();
+            return uniqueInstance;
+        }
+        private List<Harcos> _jatharcosok = new List<Harcos>();
+        public List<Harcos> jatharcosok
+        {
+            get { return _jatharcosok; }
+        }
+        private List<Magus> _jatmagusok = new List<Magus>();
+        public List<Magus> jatmagusok
+        {
+            get { return _jatmagusok; }
+        }
+        private List<Ijasz> _jatijaszok = new List<Ijasz>();
+        public List<Ijasz> jatijaszok
+        {
+            get { return _jatijaszok; }
+        }
+        private int _kivantkaszt;
+        public int kivantkaszt
+        {
+            get { return _kivantkaszt; }
+            set { _kivantkaszt = value; }
+        }
+        public void JatHarcosHozzaad(Harcos jharcos)
+        {
+            jatharcosok.Add(jharcos);
+        }
+        public void JatMagusHozzaad(Magus jmagus)
+        {
+            jatmagusok.Add(jmagus);
+        }
+        public void JatIjaszHozzaad(Ijasz jijasz)
+        {
+            jatijaszok.Add(jijasz);
+        }
+    }
+}
